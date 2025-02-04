765cf1440cbf46fd15d8987fbfd047f9dd57cf5e
Wmz1024 <wmzwsa@gmail.com>
2025-02-04 12:58:01 +0000

    Update. Darkmode

--- public/global.js
+++ public/global.js
@@ -3,3 +3,14 @@ Copyright 2025 HoRzTeam [i@horz.team]
 Computed by MZCompute GmbH. [wang@mingze.de]`)
 //const basedomain = "www.transfer.cool"
 const basedomain = location.hostname
+// 检查本地存储中的暗色模式设置
+const darkMode = localStorage.getItem('darkmode') === 'true';
+if (darkMode) {
+    document.body.classList.add('dark-mode');
+}
+
+// 切换暗色模式
+function toggleDarkMode() {
+    document.body.classList.toggle('dark-mode');
+    localStorage.setItem('darkmode', document.body.classList.contains('dark-mode'));
+}
