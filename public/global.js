02cda9b1fa5822f48c9a8987847444217dc35f2c
Wmz1024 <wmzwsa@gmail.com>
2025-02-04 15:07:51 +0000

    Prod. Domain Change

--- public/global.js
+++ public/global.js
@@ -1,8 +1,8 @@
 console.log(`HTransfer - transfer.cool
 Copyright 2025 HoRzTeam [i@horz.team]
 Computed by MZCompute GmbH. [wang@mingze.de]`)
-//const basedomain = "www.transfer.cool"
-const basedomain = location.hostname
+const basedomain = "www.transfer.cool"
+//const basedomain = location.hostname
 // 检查本地存储中的暗色模式设置
 const darkMode = localStorage.getItem('darkmode') === 'true';
 if (darkMode) {
